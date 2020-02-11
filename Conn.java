fdf18e051c3b5de6febca3081dc65819fe7f83d0
Angel <you@example.com>
2020-02-11 17:12:41 -0500

    clase 3

--- Conn.java
+++ Conn.java
@@ -9,21 +9,27 @@ public class Conn {
     public static void main(String[] args) {
         System.out.println("Hola Java\n");
         try { 
-			Class.forName("org.sqlite.JDBC");
-			String dbURL = "jdbc:sqlite:db2.db";
-			Connection conn = DriverManager.getConnection(dbURL);
+			Connection conn = connect();
 			if (conn != null) {
 				System.out.println("Connected to the database");
 				conn.close();
 			}
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
         }
     }
-    public void connect() {
-        
+    public static Connection connect() {
+		Connection conn=null;
+		try { 
+			Class.forName("org.sqlite.JDBC");
+			String dbURL = "jdbc:sqlite:db2.db";
+			conn = DriverManager.getConnection(dbURL);
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+        return conn;
     }
 
 }
