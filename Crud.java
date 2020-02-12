9d2fd8026abc83c10d0948fc381b79f64d1fe650
Angel <you@example.com>
2020-02-12 15:54:02 -0500

    ordenando

--- Crud.java
+++ Crud.java
@@ -1,23 +1,32 @@
+
 /**
  * Crud
  */
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import a.Conn;
 
 public class Crud {
 
     public static void main(String[] args) {
-        System.out.println("Hola Java\n");
-        try { 
-			Connection conn = Conn.connectSQLite();
-			if (conn != null) {
-				System.out.println("Connected to the database");
-				conn.close();
-			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+        try {
+            Connection conn = Conn.connectSQLite();
+            if (conn != null) {
+                System.out.println("Connected to the database");
+                Statement statement = conn.createStatement();
+                ResultSet rs = statement.executeQuery("select * from Person");
+                System.out.println("ID \tName" );
+                while (rs.next()) {
+                    System.out.println("" + rs.getInt("id") + " \t " + rs.getString("name"));
+                }
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
-    
+
 }
