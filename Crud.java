72ed566d1bd75fc116ae8578ba7697648007c509
Angel <you@example.com>
2020-02-13 17:17:46 -0500

    crud

--- Crud.java
+++ Crud.java
@@ -2,55 +2,23 @@
 /**
  * Crud
  */
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import a.Conn;
+import java.util.List;
 import a.Person;
 import a.PersonDAO;
 
 public class Crud {
     public static void main(String[] args) {
-
-
-        PersonDAO dao= new PersonDAO();
-        Person p= new Person();
-        p.setName("Miguel");
-        p.setCellular("999");        
-        //dao.save(p);
-
-        //Delete
-        dao.delete(5);
-
-
-        ResultSet rs = list();
-        try {
-            System.out.println("ID \tName");
-            while (rs.next()) {
-                System.out.println("" + rs.getInt("id") + " \t " + rs.getString("name"));
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        PersonDAO dao = new PersonDAO();
+        Person p = new Person();
+        p.setId(7);
+        p.setName("Miguel ANgel");
+        p.setCellular("98552333");
+        dao.save(p);
+        // dao.delete(5);
+        List<Person> list = dao.list();
+        System.out.println("ID \tName");
+        for(Person d:list){
+            System.out.println(d.getId() + "\t"+d.getName()); 
         }
-    }
-
-    public static ResultSet list() {
-        ResultSet rs = null;
-        Connection conn = Conn.connectSQLite();
-        try {
-            if (conn != null) {
-                System.out.println("Connected to the database");
-                Statement statement = conn.createStatement();
-                rs = statement.executeQuery("select * from Person");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }finally{
-            //Conn.closeSQLite(conn);
-        }
-        return rs;
-    }
-
+    }    
 }
--- a/PersonDAO.java
+++ a/PersonDAO.java
@@ -2,21 +2,64 @@ package a;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import a.Conn;
 import a.Person;
 
 public class PersonDAO {
-    public void save(Person p) {// Create and update
+    public List<Person> list() {
+        List<Person> ls = new ArrayList<Person>();
         Connection conn = Conn.connectSQLite();
         try {
             if (conn != null) {
                 Statement statement = conn.createStatement();
-                statement.executeUpdate(
-                        "insert into Person(name,cellular) values('" + p.getName() + "','" + p.getCellular() + "')");
+                ResultSet rs = statement.executeQuery("select * from Person");
+                while (rs.next()) {
+                    Person p= new Person();
+                    p.setId(rs.getInt("id"));
+                    p.setName(rs.getString("name"));
+                    p.setCellular(rs.getString("cellular"));
+                    ls.add(p);
+                }
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
+        }
+        return ls;
+    }
+
+    public void save(Person p) {// Create and update
+        Connection conn = Conn.connectSQLite();
+        String query="";
+        if (p.getId() > 0) {
+            System.out.println("Update");
+            try {
+                if (conn != null) {
+                    Statement statement = conn.createStatement();
+                    query="update Person set "+
+                        " name ='" + p.getName() + "',"+
+                        " cellular ='" + p.getCellular() + "'"+
+                        " where id= "+p.getId();
+                    statement.executeUpdate(query);
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Insert");
+            try {
+                if (conn != null) {
+                    Statement statement = conn.createStatement();
+                    statement.executeUpdate("insert into Person(name,cellular) values('" + p.getName() + "','"
+                            + p.getCellular() + "')");
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -25,11 +68,11 @@ public class PersonDAO {
         try {
             if (conn != null) {
                 Statement statement = conn.createStatement();
-                statement.executeUpdate(
-                        "delete from Person where id="+id+"");
+                statement.executeUpdate("delete from Person where id=" + id + "");
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
+
 }
